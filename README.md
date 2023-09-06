1b934e7c63e67f6db5e2dc642973eebb97451909
Nick <111998485+NicholasEng26@users.noreply.github.com>
2023-09-06 13:45:47 -0500

    test
    test

--- README.md
+++ README.md
@@ -3,12 +3,14 @@ Repository for submitting studio work
 
 # Responses:
 Students:
-1. John Doe <john.doe@wustl.edu>
-2. 
-3. 
-4. 
+1. Nick Eng <nicholas.eng@wustl.edu>
+2. Jalen Patel <jalen.p@wustl.edu>
+3. Austin Perez <a.m.perez@wustl.edu>
+4. Thien Hoang <h.thien@wustl.edu>
 
 
 Question Answers:
-2. 
+2. hello world
+3. 0
+4. std, cout, endl
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -10,6 +10,16 @@
 using namespace std;
 
 int main() {
-    cout << "hello world" << endl;
+    int a = 10;
+    int b;
+    cout << sizeof(a) << endl;
+    cout << &a << endl;
+    int* const p_a = &a;
+    char c_str[] = {'C', 'S', 'E', '\0'};
+    char* c_p = c_str;
+    cout << c_str << endl;
+    cout << p_a << endl;
+    *p_a = 100;
+    *p_a += 100;
     return 0;
 }
