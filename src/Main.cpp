e08122f95db3086be36d380d424e7ebca9d5580e
Nick <111998485+NicholasEng26@users.noreply.github.com>
2023-09-27 00:35:29 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,20 +6,33 @@
 */
 
 #include <iostream>
+#include <sstream>      // std::istringstream
 
 using namespace std;
 
 int main() {
-    int a = 10;
-    int b;
-    cout << sizeof(a) << endl;
-    cout << &a << endl;
-    int* const p_a = &a;
-    char c_str[] = {'C', 'S', 'E', '\0'};
-    char* c_p = c_str;
-    cout << c_str << endl;
-    cout << p_a << endl;
-    *p_a = 100;
-    *p_a += 100;
+    string s = "this is a sentence";
+
+    istringstream iss(s);
+
+    string s2;
+
+    iss >> s2;
+
+    cout << s2 << endl;
+//    int a = 10;
+//    int b;
+//    cout << sizeof(a) << endl;
+//    cout << &a << endl;
+//    int* const p_a = &a;
+//    char c_str[] = {'C', 'S', 'E', '\0'};
+//    char* c_p = c_str;
+//    cout << c_str << endl;
+//    cout << p_a << endl;
+//    *p_a = 100;
+//    *p_a += 100;
+
+
+
     return 0;
 }
